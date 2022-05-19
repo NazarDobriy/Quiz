1494e2c00b1c32fe039733d942d7c9c6a6177b31
Nazar <nazaranddobriy@gmail.com>
2022-05-19 16:30:00 +0300

    add lazy loading to quiz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '', loadChildren: ()=>import('../pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'quiz', loadChildren: ()=>import('../pages/quiz/quiz.module').then(m => m.QuizModule)
   }
 ];
 
