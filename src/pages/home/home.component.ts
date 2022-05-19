59623192978b341e80aec0b1ac6aa637967d1abf
Nazar <nazaranddobriy@gmail.com>
2022-05-19 14:12:12 +0300

    wrote logic for open drop-dowm menu

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  public isMenuClicked: boolean = false;
 
   constructor() { }
 
-  ngOnInit(): void {
+  public openDropDownMenu(): void {
+    this.isMenuClicked ? this.isMenuClicked = false : this.isMenuClicked = true;
   }
 
 }
