2f81462fcfb7cf46dffd9a7897b6a26eac15971f
Nazar <nazaranddobriy@gmail.com>
2022-05-19 15:55:13 +0300

    delete path to scss file and add void type

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -2,15 +2,14 @@ import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  templateUrl: './home.component.html'
 })
 export class HomeComponent {
   public isMenuClicked: boolean = false;
   readonly modileWidth: number = 640;
 
   @HostListener('window:resize', ['$event'])
-  public onResize() {
+  public onResize(): void {
     if (window.innerWidth > this.modileWidth) {
       this.isMenuClicked = false;
     }
