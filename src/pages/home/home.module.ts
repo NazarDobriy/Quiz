f499d6bdfc14e5cb89c342c002e7404115cc5057
Nazar <nazaranddobriy@gmail.com>
2022-05-23 17:53:16 +0300

    fix url

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizzesComponent } from '../quiz/quizzes.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'quizzes', component: QuizzesComponent }
 ];
 
 @NgModule({
