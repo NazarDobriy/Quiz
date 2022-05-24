065f23ac0fe5b118325834c7740a93f175d18a28
Nazar <nazaranddobriy@gmail.com>
2022-05-24 16:42:47 +0300

    fix max width

--- src/pages/quiz/quizzes.component.ts
+++ src/pages/quiz/quizzes.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ICardTheme, QuizService, IQuiz } from './providers/quiz.service';
 
 @Component({
@@ -17,22 +17,8 @@ export class QuizzesComponent implements OnInit {
     this.cardThemes = this.quizService.cardThemes;
   }
 
-  @HostListener('window:resize', ['$event'])
-  public onResize(): void {
-    if(window.innerWidth < 1400) {
-      const separators: NodeListOf<HTMLElement> = document.querySelectorAll('.separator');
-      separators.forEach((separator: HTMLElement) => {
-        separator.style.display = 'none';
-      });
-    }
-  }
-
   public loadCards(): void {
     this.quizCards = [...this.quizCards, ...this.quizService.getQuizzes()];
-  }
-
-  public isFullCardsRow(index: number = 0): boolean {
-    return (index + 1) % this.MAX_CARDS_IN_ROW === 0;
   }
 
 }
