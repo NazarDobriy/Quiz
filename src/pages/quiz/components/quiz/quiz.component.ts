115d45b666ca43875838f796de1bd02b554ffef8
Nazar <nazaranddobriy@gmail.com>
2022-05-24 12:53:12 +0300

    take id from route

--- src/pages/quiz/components/quiz/quiz.component.ts
+++ src/pages/quiz/components/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-quiz',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiz.component.scss']
 })
 export class QuizComponent implements OnInit {
+  public quizId!: number;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.quizId = parseInt(this.activatedRoute.snapshot.params['id']);
   }
 
 }
