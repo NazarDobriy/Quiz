13fd1f87f30f8a1d8e79a574e093793dbf2b7bdf
Nazar <nazaranddobriy@gmail.com>
2022-06-02 14:32:31 +0300

    format QuizExitDialogComponent

--- src/pages/quiz/components/quiz-dialog/quiz-exit-dialog.component.ts
+++ src/pages/quiz/components/quiz-dialog/quiz-exit-dialog.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quiz-exit-dialog',
@@ -10,10 +9,7 @@ import { Router } from '@angular/router';
 export class QuizExitDialogComponent implements OnInit {
   public title: string = '';
 
-  constructor(
-    private router: Router,
-    @Inject(MAT_DIALOG_DATA) private data: string
-  ) {}
+  constructor(@Inject(MAT_DIALOG_DATA) private data: string) {}
 
   ngOnInit(): void {
     this.title = this.data;
