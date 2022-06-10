71fb475d8e98ce3efd868f23928693574967e945
Nazar <nazaranddobriy@gmail.com>
2022-06-10 18:29:48 +0300

    add quiz-data with themes

---
+++ src/pages/quizzes/quiz-data.ts
@@ -0,0 +1,74 @@
+import { IQuizTheme } from "./providers/theme.service";
+
+export const QUIZ_THEMES: IQuizTheme[] = [
+  {
+    titleTextClass: 'text-primary',
+    primaryTextClass: 'text-bright',
+    secondaryTextClass: 'text-primary',
+    secondaryActiveTextClass: 'text-bright',
+    numberTextClass: 'text-primary',
+    numberBackgroundClass: 'bg-warning',
+    backgroundClass: 'bg-success',
+    btnsBackgroundClass: 'bg-accent',
+    btnsTextClass: 'text-bright',
+    radioButtonColor: 'accent-error',
+    iconSrc: 'assets/img/Mili.svg',
+    personName: 'Mili'
+  },
+  {
+    titleTextClass: 'text-primary',
+    primaryTextClass: 'text-bright',
+    secondaryTextClass: 'text-primary',
+    secondaryActiveTextClass: 'text-bright',
+    numberTextClass: 'text-primary',
+    numberBackgroundClass: 'bg-warning',
+    backgroundClass: 'bg-error',
+    btnsBackgroundClass: 'bg-accent',
+    btnsTextClass: 'text-bright',
+    radioButtonColor: 'accent-accent',
+    iconSrc: 'assets/img/Meg.svg',
+    personName: 'Meg'
+  },
+  {
+    titleTextClass: 'text-secondary',
+    primaryTextClass: 'text-bright',
+    secondaryTextClass: 'text-primary',
+    secondaryActiveTextClass: 'text-bright',
+    numberTextClass: 'text-primary',
+    numberBackgroundClass: 'bg-warning',
+    backgroundClass: 'bg-accent',
+    btnsBackgroundClass: 'bg-error',
+    btnsTextClass: 'text-bright',
+    radioButtonColor: 'accent-error',
+    iconSrc: 'assets/img/Steven.svg',
+    personName: 'Steven'
+  },
+  {
+    titleTextClass: 'text-secondary',
+    primaryTextClass: 'text-primary',
+    secondaryTextClass: 'text-primary',
+    secondaryActiveTextClass: 'text-bright',
+    numberTextClass: 'text-primary',
+    numberBackgroundClass: 'bg-warning',
+    backgroundClass: 'bg-bright',
+    btnsBackgroundClass: 'bg-error',
+    btnsTextClass: 'text-bright',
+    radioButtonColor: 'accent-error',
+    iconSrc: 'assets/img/Mili.svg',
+    personName: 'Mili'
+  },
+  {
+    titleTextClass: 'text-secondary',
+    primaryTextClass: 'text-primary',
+    secondaryTextClass: 'text-primary',
+    secondaryActiveTextClass: 'text-accent',
+    numberTextClass: 'text-bright',
+    numberBackgroundClass: 'bg-error',
+    backgroundClass: 'bg-warning',
+    btnsBackgroundClass: 'bg-accent',
+    btnsTextClass: 'text-primary',
+    radioButtonColor: 'accent-error',
+    iconSrc: 'assets/img/Steven.svg',
+    personName: 'Steven'
+  }
+];
