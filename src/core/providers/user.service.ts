7ee039cfc8f33520dcc2172d0b54e240a5a7e411
Nazar <nazaranddobriy@gmail.com>
2022-06-13 23:26:21 +0300

    operations with uuid in user service

--- src/core/providers/user.service.ts
+++ src/core/providers/user.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 import { LocalStorageService } from './local-storage.service';
 
 @Injectable()
 export class UserService {
 
   constructor(private localStorageService: LocalStorageService) { }
+
+  get id(): string | null {
+    return this.localStorageService.get("uuid");
+  }
+
+  get hasId(): boolean {
+    return this.localStorageService.has("uuid");
+  }
+
+  public generateId(): void {
+    this.localStorageService.set("uuid", uuidv4());
+  }
+
 }
