229b7919c33a42778ad4efb8569323d040a16d16
Nazar <nazaranddobriy@gmail.com>
2022-06-13 22:58:16 +0300

    install uuid package and add user service in constructor app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/core/providers/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Quiz';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    
+  }
+
 }
