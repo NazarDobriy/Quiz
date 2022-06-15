545a2c9c411102d193773d7de55ca3ff694028b3
Nazar <nazaranddobriy@gmail.com>
2022-06-15 11:31:58 +0300

    fill chart

--- src/pages/quizzes/pages/active/components/doughnut-chart/doughnut-chart.component.ts
+++ src/pages/quizzes/pages/active/components/doughnut-chart/doughnut-chart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-doughnut-chart',
@@ -6,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoughnutChartComponent implements OnInit {
 
-  constructor() { }
+  private options = {
+    animation: {
+      animateRotate: false
+    }
+  };
+
+  private data = {
+    datasets: [{
+      data: [300, 50, 100],
+      backgroundColor: [
+        '#FEFD54',
+        '#63C995',
+        '#3545E9'
+      ],
+      hoverOffset: 4
+    }]
+  };
 
   ngOnInit(): void {
+    const canvas: HTMLCanvasElement = document.getElementById('myChart') as HTMLCanvasElement;
+    if (canvas != null) {
+      const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
+      if (ctx != null) {
+        new Chart(ctx, {
+          type: 'doughnut',
+          data: this.data,
+          options: this.options
+        });
+      }
+    }
   }
 
 }
