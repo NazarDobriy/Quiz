b5623c77bec3e42bacc21220e962c938b41c89f7
Nazar <nazaranddobriy@gmail.com>
2022-06-15 11:10:11 +0300

    add doughnut-chart component

---
+++ src/pages/quizzes/pages/active/components/doughnut-chart/doughnut-chart.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doughnut-chart',
+  templateUrl: './doughnut-chart.component.html'
+})
+export class DoughnutChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
