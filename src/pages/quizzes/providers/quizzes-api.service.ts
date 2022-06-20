aa65a1445b0e0764920728838fb7baf616157836
Nazar <nazaranddobriy@gmail.com>
2022-06-20 13:02:48 +0300

    return duration

--- src/pages/quizzes/providers/quizzes-api.service.ts
+++ src/pages/quizzes/providers/quizzes-api.service.ts
@@ -35,13 +35,13 @@ export class QuizzesApiService {
     ));
   }
 
-  public setQuizAnswers(quizId: number, answers: string[], correctAnswers: number, duration: number): void {
+  public setQuizAnswers(quizId: number, answers: string[], correctAnswers: number, duration: Duration): void {
     if (this.userService.id) {
       const path: string = `quiz_answers/${this.userService.id}/${quizId}`;
       this.db.object<IQuizResult>(path).set({
         answers: answers,
         correct: correctAnswers,
-        seconds: duration
+        seconds: duration.seconds
       });
     }
   }
