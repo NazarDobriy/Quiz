d44ee54782ae3546306575069dd821542c79ca50
Nazar <nazaranddobriy@gmail.com>
2022-06-21 11:09:15 +0300

    add common module to core

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { UserService } from './providers/user.service';
 import { LocalStorageService } from './providers/local-storage.service';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [
     HeaderComponent
+  ],
+  imports: [
+    CommonModule
   ],
   exports: [
     HeaderComponent
