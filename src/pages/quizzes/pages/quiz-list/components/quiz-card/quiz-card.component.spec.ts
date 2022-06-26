aa9e92ceadb898d08d979209da2f973eb38c42af
NazarFront <nazarii.dobrii@nerdysoft.com>
2022-06-26 12:35:00 +0300

    write tests

--- src/pages/quizzes/pages/quiz-list/components/quiz-card/quiz-card.component.spec.ts
+++ src/pages/quizzes/pages/quiz-list/components/quiz-card/quiz-card.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { mockQuizThemes, mockQuizzes } from 'src/mock-data';
+import { ThemeService } from 'src/pages/quizzes/providers/theme.service';
 
 import { QuizCardComponent } from './quiz-card.component';
 
@@ -6,8 +8,17 @@ describe('QuizCardComponent', () => {
   let component: QuizCardComponent;
   let fixture: ComponentFixture<QuizCardComponent>;
 
+  let mockThemeService: jasmine.SpyObj<ThemeService>;
+
   beforeEach(async () => {
+    mockThemeService = jasmine.createSpyObj(['getThemeByText']);
     await TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: ThemeService,
+          useValue: mockThemeService
+        }
+      ],
       declarations: [ QuizCardComponent ]
     })
     .compileComponents();
@@ -16,10 +27,16 @@ describe('QuizCardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(QuizCardComponent);
     component = fixture.componentInstance;
+    component.quiz = mockQuizzes[0];
+    mockThemeService.getThemeByText.and.returnValue(mockQuizThemes[0]);
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get theme', () => {
+    expect(component.theme).toEqual(mockQuizThemes[0]);
   });
 });
