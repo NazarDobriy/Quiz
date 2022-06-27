3b3f3db5ba8e1db542c817623ee61a63ceb763bb
NazarFront <nazarii.dobrii@nerdysoft.com>
2022-06-27 11:44:06 +0300

    replace mock data

--- src/pages/quizzes/pages/active/providers/dialog.service.spec.ts
+++ src/pages/quizzes/pages/active/providers/dialog.service.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { mockDialogConfig } from 'src/mock-data';
 import { ConfirmDialogComponent } from '../components/confirm-dialog/confirm-dialog.component';
 
-import { DialogService } from './dialog.service';
+import { DialogService, IDialogConfig } from './dialog.service';
 
 describe('DialogService', () => {
   let service: DialogService;
   let mockMatDialog: jasmine.SpyObj<MatDialog>;
+
+  const mockDialogConfig: IDialogConfig = {
+    title: "Cancel",
+    message: "Are you sure you want to exit?",
+    dismissText: "No",
+    confirmText: "Yes"
+  };
 
   beforeEach(() => {
     mockMatDialog = jasmine.createSpyObj(['open']);
