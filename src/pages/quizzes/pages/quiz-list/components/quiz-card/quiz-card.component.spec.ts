c46cf3976dbbad763c96c9f129aa4d2900245f8a
NazarFront <nazarii.dobrii@nerdysoft.com>
2022-06-27 12:41:28 +0300

    mock template app-icon

--- src/pages/quizzes/pages/quiz-list/components/quiz-card/quiz-card.component.spec.ts
+++ src/pages/quizzes/pages/quiz-list/components/quiz-card/quiz-card.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AngularSvgIconModule, SvgIconRegistryService, SvgLoader } from 'angular-svg-icon';
 import { mockQuizThemes, mockQuizzes } from 'src/mock-data';
 import { ThemeService } from 'src/pages/quizzes/providers/theme.service';
+import { IconComponent } from 'src/shared/components/icon/icon.component';
 
 import { QuizCardComponent } from './quiz-card.component';
 
@@ -9,17 +12,37 @@ describe('QuizCardComponent', () => {
   let fixture: ComponentFixture<QuizCardComponent>;
 
   let mockThemeService: jasmine.SpyObj<ThemeService>;
+  let mockSvgLoader: jasmine.SpyObj<SvgLoader>;
+  let mockSvgIconRegistryService: jasmine.SpyObj<SvgIconRegistryService>;
 
   beforeEach(async () => {
     mockThemeService = jasmine.createSpyObj(['getThemeByText']);
+    mockSvgLoader = jasmine.createSpyObj(['getSvg']);
+    mockSvgIconRegistryService = jasmine.createSpyObj(['loadSvg']);
+
     await TestBed.configureTestingModule({
+      imports: [
+        AngularSvgIconModule,
+        RouterTestingModule
+      ],
       providers: [
         {
           provide: ThemeService,
           useValue: mockThemeService
+        },
+        {
+          provide: SvgIconRegistryService,
+          useValue: mockSvgIconRegistryService
+        },
+        {
+          provide: SvgLoader,
+          useValue: mockSvgLoader
         }
       ],
-      declarations: [ QuizCardComponent ]
+      declarations: [
+        QuizCardComponent,
+        IconComponent
+      ]
     })
     .compileComponents();
   });
