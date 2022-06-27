3a64587de3412dee642d56032365a1ce95ed9e33
NazarFront <nazarii.dobrii@nerdysoft.com>
2022-06-27 14:46:23 +0300

    cover whole template with tests

--- src/pages/quizzes/pages/quiz-list/components/quiz-card/quiz-card.component.spec.ts
+++ src/pages/quizzes/pages/quiz-list/components/quiz-card/quiz-card.component.spec.ts
@@ -50,39 +50,65 @@ describe('QuizCardComponent', () => {
     expect(component.theme).toEqual(mockQuizThemes[0]);
   });
 
-  describe('template title', () => {
+  describe('Template', () => {
     let titleDebugElem: DebugElement;
     let titleElem: HTMLElement;
 
-    beforeEach(() => {
-      titleDebugElem = fixture.debugElement.query(By.css(".title"));
-      titleElem = titleDebugElem.nativeElement;
-    });
-
-    it('should check new added class', () => {
-      expect(titleElem.classList).toContain('text-primary');
-    });
-
-    it('should check text', () => {
-      expect(titleElem.textContent).toBe('Music is the best');
-    });
-  });
-  
-  describe('template app-icon', () => {
     let appIconDebugElem: DebugElement;
     let appIconNode: DebugNode["nativeNode"];
 
+    let subTitleDebugElem: DebugElement;
+    let subTitleElem: HTMLElement;
+
+    let routeDebugElem: DebugElement;
+    let routeElem: HTMLAnchorElement;
+
+    let articleDebugElem: DebugElement;
+    let articleElem: HTMLElement;
+
     beforeEach(() => {
+      routeDebugElem = fixture.debugElement.query(By.css("a"));
+      routeElem = routeDebugElem.nativeElement;
+      articleDebugElem = fixture.debugElement.query(By.css("article"));
+      articleElem = articleDebugElem.nativeElement;
+      titleDebugElem = fixture.debugElement.query(By.css(".title"));
+      titleElem = titleDebugElem.nativeElement;
       appIconDebugElem = fixture.debugElement.query(By.css("app-icon"));
       appIconNode = appIconDebugElem.nativeNode;
+      subTitleDebugElem = fixture.debugElement.query(By.css(".subtitle"));
+      subTitleElem = subTitleDebugElem.nativeElement;
     });
 
-    it('should check icon name', () => {
+    it('should check routerLink', () => {
+      expect(routeElem.href).toBe(window.location.origin + "/quizzes/active/1");
+    });
+
+    it('should article has new class', () => {
+      expect(articleElem.classList).toContain('bg-success');
+    });
+
+    it('should title elem has new class', () => {
+      expect(titleElem.classList).toContain('text-primary');
+    });
+
+    it('should check title elem text', () => {
+      expect(titleElem.textContent).toBe('Music is the best');
+    });
+
+    it('should check app icon name', () => {
       expect(appIconNode.iconName).toBe('mili');
     });
 
-    it('should check classes', () => {
+    it('should check app icon classes', () => {
       expect(appIconNode.styleClass).toBe('h-12 w-12 rounded-full');
+    });
+
+    it('should subtitle elem has new class', () => {
+      expect(subTitleElem.classList).toContain('text-bright');
+    });
+
+    it('should check subtitle elem text', () => {
+      expect(subTitleElem.textContent).toBe('Music questions');
     });
   });
 });
