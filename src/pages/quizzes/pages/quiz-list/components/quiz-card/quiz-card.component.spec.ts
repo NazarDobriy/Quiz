e78096faab8519780020e3f76fef8849495c3004
NazarFront <nazarii.dobrii@nerdysoft.com>
2022-06-27 13:38:53 +0300

    test template <p> title

--- src/pages/quizzes/pages/quiz-list/components/quiz-card/quiz-card.component.spec.ts
+++ src/pages/quizzes/pages/quiz-list/components/quiz-card/quiz-card.component.spec.ts
@@ -1,9 +1,9 @@
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AngularSvgIconModule, SvgIconRegistryService, SvgLoader } from 'angular-svg-icon';
 import { mockQuizThemes, mockQuizzes } from 'src/mock-data';
 import { ThemeService } from 'src/pages/quizzes/providers/theme.service';
-import { IconComponent } from 'src/shared/components/icon/icon.component';
 
 import { QuizCardComponent } from './quiz-card.component';
 
@@ -12,37 +12,24 @@ describe('QuizCardComponent', () => {
   let fixture: ComponentFixture<QuizCardComponent>;
 
   let mockThemeService: jasmine.SpyObj<ThemeService>;
-  let mockSvgLoader: jasmine.SpyObj<SvgLoader>;
-  let mockSvgIconRegistryService: jasmine.SpyObj<SvgIconRegistryService>;
 
   beforeEach(async () => {
     mockThemeService = jasmine.createSpyObj(['getThemeByText']);
-    mockSvgLoader = jasmine.createSpyObj(['getSvg']);
-    mockSvgIconRegistryService = jasmine.createSpyObj(['loadSvg']);
 
     await TestBed.configureTestingModule({
       imports: [
-        AngularSvgIconModule,
         RouterTestingModule
       ],
       providers: [
         {
           provide: ThemeService,
           useValue: mockThemeService
-        },
-        {
-          provide: SvgIconRegistryService,
-          useValue: mockSvgIconRegistryService
-        },
-        {
-          provide: SvgLoader,
-          useValue: mockSvgLoader
         }
       ],
       declarations: [
-        QuizCardComponent,
-        IconComponent
-      ]
+        QuizCardComponent
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   });
@@ -61,5 +48,23 @@ describe('QuizCardComponent', () => {
 
   it('should get theme', () => {
     expect(component.theme).toEqual(mockQuizThemes[0]);
+  });
+
+  describe('template title', () => {
+    let titleDebugElem: DebugElement;
+    let titleElem: HTMLElement;
+
+    beforeEach(() => {
+      titleDebugElem = fixture.debugElement.query(By.css(".title"));
+      titleElem = titleDebugElem.nativeElement;
+    });
+
+    it('should check new added class', () => {
+      expect(titleElem.classList).toContain('text-primary');
+    });
+
+    it('should check text', () => {
+      expect(titleElem.textContent).toBe('Music is the best');
+    });
   });
 });
