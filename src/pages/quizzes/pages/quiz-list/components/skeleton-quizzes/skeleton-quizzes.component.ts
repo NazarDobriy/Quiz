d883e7315af640a9b4c0306d2408cbe114f19748
NazarFront <nazarii.dobrii@nerdysoft.com>
2022-06-29 22:52:47 +0300

    add limit of cards

--- src/pages/quizzes/pages/quiz-list/components/skeleton-quizzes/skeleton-quizzes.component.ts
+++ src/pages/quizzes/pages/quiz-list/components/skeleton-quizzes/skeleton-quizzes.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-skeleton-quizzes',
   templateUrl: './skeleton-quizzes.component.html'
 })
-export class SkeletonQuizzesComponent {}
+export class SkeletonQuizzesComponent {
+  @Input() limit: number = 0;
+}
