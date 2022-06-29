8fed0f7c593413212858e392c15b814a9534986f
NazarFront <nazarii.dobrii@nerdysoft.com>
2022-06-30 00:26:57 +0300

    add quiz skeleton

---
+++ src/pages/quizzes/pages/active/components/skeleton-quiz/skeleton-quiz.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SkeletonQuizComponent } from './skeleton-quiz.component';
+
+describe('SkeletonQuizComponent', () => {
+  let component: SkeletonQuizComponent;
+  let fixture: ComponentFixture<SkeletonQuizComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SkeletonQuizComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SkeletonQuizComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/pages/quizzes/pages/active/components/skeleton-quiz/skeleton-quiz.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-quiz',
+  templateUrl: './skeleton-quiz.component.html'
+})
+export class SkeletonQuizComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
