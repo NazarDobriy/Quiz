cec71e03ca8af8afaf0d468d6ca6e223ad1fd3c5
Nazar <nazaranddobriy@gmail.com>
2022-07-18 16:25:00 +0300

    write tests for LocalStorageServer

--- src/core/providers/local-storage.server.spec.ts
+++ src/core/providers/local-storage.server.spec.ts
@@ -1,20 +1,66 @@
+import { PLATFORM_ID } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 
 import { LocalStorageServer } from './local-storage.server';
+import { PlatformService } from './platform.service';
 
 
 describe('LocalStorageServer', () => {
   let service: LocalStorageServer;
 
+  const mockPlatformService = {
+    get isBrowser(): boolean {
+      return true;
+    }
+  };
+
+  let itemKey: string = 'car';
+  let itemValue: string = 'Ford';
+
   beforeEach(() => {
+    localStorage.setItem(itemKey, itemValue);
     TestBed.configureTestingModule({
-      providers: [LocalStorageServer]
+      providers: [
+        LocalStorageServer,
+        {
+          provide: PlatformService,
+          useValue: mockPlatformService
+        }
+      ]
     });
     service = TestBed.inject(LocalStorageServer);
   });
 
-  xit('should be created', () => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get value by key', () => {
+    expect(service.get(itemKey)).toEqual(itemValue);
+  });
+
+  it('should check get key after delete', () => {
+    localStorage.removeItem(itemKey);
+    expect(service.get(itemKey)).toEqual(null);
+  });
+
+  it('should check value by key', () => {
+    expect(service.has(itemKey)).toBeTruthy();
+  });
+
+  it('should set value by key', () => {
+    service.set(itemKey, 'Tesla');
+    expect(localStorage.getItem(itemKey)).toEqual('Tesla');
+  });
+
+  it('should remove value by key', () => {
+    service.remove(itemKey);
+    expect(localStorage.getItem(itemKey)).toEqual(null);
+  });
+
+  it('should clear storage', () => {
+    service.clear();
+    expect(localStorage.length).toBe(0);
   });
 
 });
