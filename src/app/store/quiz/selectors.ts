41a27db00fa3815483e85273f303e567031fff23
Nazar <nazaranddobriy@gmail.com>
2022-09-23 09:40:49 +0300

    feat: add selectors for quiz

--- src/app/store/quiz/selectors.ts
+++ src/app/store/quiz/selectors.ts
@@ -1,22 +1,21 @@
-import { createSelector } from "@ngrx/store";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
 
 import { IPaginationScheme, IQuiz, IQuizResult } from "src/pages/quizzes/providers/quiz.service";
 import { IQuizTheme } from "src/pages/quizzes/providers/theme.service";
-import { State } from "../state";
 import { QuizState } from "./state";
 
-export const quizState = (state: State): QuizState => state.quiz;
+export const selectQuizFeature = createFeatureSelector<QuizState>('quiz');
 
-export const selectQuiz = createSelector(quizState, (state: QuizState): IQuiz => state.quiz);
+export const selectQuiz = createSelector(selectQuizFeature, (state: QuizState) => state.quiz);
 
-export const selectQuizzes = createSelector(quizState, (state: QuizState): IQuiz[] => state.quizzes);
+export const selectQuizzes = createSelector(selectQuizFeature, (state: QuizState): IQuiz[] => state.quizzes);
 
-export const selectTheme = createSelector(quizState, (state: QuizState): IQuizTheme => state.theme);
+export const selectTheme = createSelector(selectQuizFeature, (state: QuizState): IQuizTheme => state.theme);
 
-export const selectThemes = createSelector(quizState, (state: QuizState): IQuizTheme[] => state.themes);
+export const selectThemes = createSelector(selectQuizFeature, (state: QuizState): IQuizTheme[] => state.themes);
 
-export const selectCorrectAnswers = createSelector(quizState, (state: QuizState): string[] => state.correctAnswers);
+export const selectCorrectAnswers = createSelector(selectQuizFeature, (state: QuizState): string[] => state.correctAnswers);
 
-export const selectResult = createSelector(quizState, (state: QuizState): IQuizResult | null => state.result);
+export const selectResult = createSelector(selectQuizFeature, (state: QuizState): IQuizResult | null => state.result);
 
-export const selectPaginationScheme = createSelector(quizState, (state: QuizState): IPaginationScheme<IQuiz> => state.paginationScheme);
+export const selectPaginationScheme = createSelector(selectQuizFeature, (state: QuizState): IPaginationScheme<IQuiz> => state.paginationScheme);
