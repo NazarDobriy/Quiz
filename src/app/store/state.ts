0391564281115ed89755412f84a8ee0ecea89bf6
Nazar <nazaranddobriy@gmail.com>
2022-10-22 20:12:50 +0300

    refactor: update main state with new results state

--- src/app/store/state.ts
+++ src/app/store/state.ts
@@ -1,5 +1,7 @@
 import { IQuizzesState } from "./quizzes/state";
+import { IResultsState } from "./results/state";
 
 export interface IAppState {
   quizzes: IQuizzesState;
+  results: IResultsState;
 }
