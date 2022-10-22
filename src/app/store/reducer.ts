574c4f5fe613d2a53c4225a094a34c906e89c553
Nazar <nazaranddobriy@gmail.com>
2022-10-22 20:12:23 +0300

    refactor: update main reducer with new results reducer

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -2,10 +2,12 @@ import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 
 import { environment } from '../../environments/environment';
 import { quizzesReducer } from './quizzes/reducer';
+import { resultsReducer } from './results/reducer';
 import { IAppState } from './state';
 
 export const reducers: ActionReducerMap<IAppState> = {
-  quizzes: quizzesReducer
+  quizzes: quizzesReducer,
+  results: resultsReducer
 };
 
 export const metaReducers: MetaReducer<IAppState>[] = !environment.production ? [] : [];
