b272f6aa6bcf43ebee148f2a7c0f7feb16cc13a7
Nazar <nazaranddobriy@gmail.com>
2022-10-22 20:13:28 +0300

    feat: add results state in store service

--- src/core/providers/store.service.ts
+++ src/core/providers/store.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
-import { IQuiz } from 'src/pages/quizzes/providers/quiz.service';
+import { IQuiz, IQuizResult } from 'src/pages/quizzes/providers/quiz.service';
 import * as QuizzesActions from '../../app/store/quizzes/actions';
 import * as QuizzesSelectors from '../../app/store/quizzes/selectors';
+import * as ResultsActions from '../../app/store/results/actions';
+import * as ResultsSelectors from '../../app/store/results/selectors';
 
 @Injectable()
 export class StoreService {
@@ -12,9 +14,17 @@ export class StoreService {
   public quizzesError$: Observable<string | null> = this.store.select(QuizzesSelectors.selectError);
   public isLoadingQuizzes$: Observable<boolean> = this.store.select(QuizzesSelectors.selectIsLoading);
 
+  public results$: Observable<IQuizResult[]> = this.store.select(ResultsSelectors.selectResults);
+  public resultsError$: Observable<string | null> = this.store.select(ResultsSelectors.selectError);
+  public isLoadingResults$: Observable<boolean> = this.store.select(ResultsSelectors.selectIsLoading);
+
   constructor(private store: Store) { }
 
   public dispatchQuizzes(): void {
     this.store.dispatch(QuizzesActions.getQuizzes());
+  }
+
+  public dispatchResults(): void {
+    this.store.dispatch(ResultsActions.getResults());
   }
 }
