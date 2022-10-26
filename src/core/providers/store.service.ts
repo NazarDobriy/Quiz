65d769bd8e943ad49196fb7e976ca85ea048714f
Nazar <nazaranddobriy@gmail.com>
2022-10-26 23:36:37 +0300

    feat: add pagination state variables in store service

--- src/core/providers/store.service.ts
+++ src/core/providers/store.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
-import { IQuiz, IQuizResult } from 'src/pages/quizzes/providers/quiz.service';
+import { IPaginationScheme, IQuiz, IQuizResult } from 'src/pages/quizzes/providers/quiz.service';
 import * as QuizzesActions from '../../app/store/quizzes/actions';
 import * as QuizzesSelectors from '../../app/store/quizzes/selectors';
 import * as ResultsActions from '../../app/store/results/actions';
 import * as ResultsSelectors from '../../app/store/results/selectors';
+import * as PaginationActions from '../../app/store/pagination/actions';
+import * as PaginationSelectors from '../../app/store/pagination/selectors';
 
 @Injectable()
 export class StoreService {
@@ -18,6 +20,10 @@ export class StoreService {
   public resultsError$: Observable<string | null> = this.store.select(ResultsSelectors.selectError);
   public isLoadingResults$: Observable<boolean> = this.store.select(ResultsSelectors.selectIsLoading);
 
+  public paginationQuizzes$: Observable<IPaginationScheme<IQuiz>> = this.store.select(PaginationSelectors.selectPaginationQuizzes);
+  public paginationQuizzesError$: Observable<string | null> = this.store.select(PaginationSelectors.selectError);
+  public isLoadingPaginationQuizzes$: Observable<boolean> = this.store.select(PaginationSelectors.selectIsLoading);
+
   constructor(private store: Store) { }
 
   public dispatchQuizzes(): void {
@@ -26,5 +32,9 @@ export class StoreService {
 
   public dispatchResults(): void {
     this.store.dispatch(ResultsActions.getResults());
+  }
+
+  public dispatchPaginationQuizzes(offset: number, count: number): void {
+    this.store.dispatch(PaginationActions.getPaginationQuizzes({ offset: offset, count: count}));
   }
 }
