3e4edcaf7da76cf785e299f91344541a80e81ddd
Nazar <nazaranddobriy@gmail.com>
2022-10-26 23:35:11 +0300

    fix: main state and reducer adding new pagination

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -1,13 +1,15 @@
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 
 import { environment } from '../../environments/environment';
+import { paginationReducer } from './pagination/reducer';
 import { quizzesReducer } from './quizzes/reducer';
 import { resultsReducer } from './results/reducer';
 import { IAppState } from './state';
 
 export const reducers: ActionReducerMap<IAppState> = {
   quizzes: quizzesReducer,
-  results: resultsReducer
+  results: resultsReducer,
+  pagination: paginationReducer
 };
 
 export const metaReducers: MetaReducer<IAppState>[] = !environment.production ? [] : [];
--- src/app/store/state.ts
+++ src/app/store/state.ts
@@ -1,7 +1,10 @@
+import { IQuiz } from "src/pages/quizzes/providers/quiz.service";
+import { IPaginationState } from "./pagination/state";
 import { IQuizzesState } from "./quizzes/state";
 import { IResultsState } from "./results/state";
 
 export interface IAppState {
   quizzes: IQuizzesState;
   results: IResultsState;
+  pagination: IPaginationState<IQuiz>;
 }
