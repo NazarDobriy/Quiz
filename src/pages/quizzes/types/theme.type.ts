b76dc2e3d936e220878545f54d4165cb0d782af7
Nazar <nazaranddobriy@gmail.com>
2023-03-23 12:30:16 +0200

    feat: create a file with theme interface

---
+++ src/pages/quizzes/types/theme.type.ts
@@ -0,0 +1,13 @@
+export interface IQuizTheme {
+  primaryTextClass: string;
+  secondaryTextClass: string;
+  secondaryActiveTextClass: string;
+  numberTextClass: string;
+  numberBackgroundClass: string;
+  backgroundClass: string;
+  btnsBackgroundClass: string;
+  btnsTextClass: string;
+  radioButtonColor: string;
+  titleTextClass: string;
+  personName: string;
+}
