80740e4cf056556de45eae95aa7733f816acff81
Nazar <nazaranddobriy@gmail.com>
2024-02-22 20:10:51 +0200

    fix: quiz guard unit tests

--- src/pages/quizzes/pages/active/providers/quiz-guard.service.spec.ts
+++ src/pages/quizzes/pages/active/providers/quiz-guard.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
+import { DialogService } from './dialog.service';
 import { QuizGuardService } from './quiz-guard.service';
+import { QuizService } from '@a-pages/quizzes/providers/quiz.service';
+import { MockQuizService } from '@a-tests/providers/mock-quiz.service';
 
 describe('QuizGuardService', () => {
   let service: QuizGuardService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        QuizGuardService,
+        { provide: DialogService, useValue: {} },
+        { provide: QuizService, useClass: MockQuizService },
+      ],
+    });
     service = TestBed.inject(QuizGuardService);
   });
 
