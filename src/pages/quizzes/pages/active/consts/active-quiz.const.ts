34273d22be2814fc815f734aa11a0b7f1f787b10
Nazar <nazaranddobriy@gmail.com>
2024-02-24 21:50:42 +0200

    feat: carry out initial quiz theme constant

---
+++ src/pages/quizzes/pages/active/consts/active-quiz.const.ts
@@ -0,0 +1,15 @@
+import { IQuizTheme } from '@a-pages/quizzes/types/theme.type';
+
+export const initialQuizTheme: IQuizTheme = {
+  primaryTextClass: '',
+  secondaryTextClass: '',
+  secondaryActiveTextClass: '',
+  numberTextClass: '',
+  numberBackgroundClass: '',
+  backgroundClass: '',
+  btnsBackgroundClass: '',
+  btnsTextClass: '',
+  radioButtonColor: '',
+  titleTextClass: '',
+  personName: ''
+};
