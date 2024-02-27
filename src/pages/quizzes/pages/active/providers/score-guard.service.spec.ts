ff1fe4eb73a396f523c28e757fab7269129740db
Nazar <nazaranddobriy@gmail.com>
2024-02-27 12:06:13 +0200

    fix: unit tests for ScoreGuardService

--- src/pages/quizzes/pages/active/providers/score-guard.service.spec.ts
+++ src/pages/quizzes/pages/active/providers/score-guard.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ScoreGuardService } from './score-guard.service';
+import { QuizStoreService } from '@a-core/providers/quiz-store.service';
 
 describe('ScoreGuardService', () => {
   let service: ScoreGuardService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        ScoreGuardService,
+        { provide: QuizStoreService, useValue: {} }
+      ]
+    });
     service = TestBed.inject(ScoreGuardService);
   });
 
